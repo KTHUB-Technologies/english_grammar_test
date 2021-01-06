52452f08f553c9ca0285af00f4176a001357edb7
ThanhTong2810 <tong.duong@kthub.dev>
2021-01-06 13:16:25 +0700

    error config android

--- android/app/src/main/kotlin/com/example/the_enest_english_grammar_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/the_enest_english_grammar_test/MainActivity.kt
@@ -1,6 +1,18 @@
 package com.example.the_enest_english_grammar_test
 
-import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
 }
+
+
+
+//import io.flutter.embedding.android.FlutterActivity
+//
+//class MainActivity: FlutterActivity() {}
--- lib/helper/sounds_helper.dart
+++ lib/helper/sounds_helper.dart
@@ -7,12 +7,11 @@ class SoundsHelper{
   static AudioPlayer audioPlayer;
 
   static Future<void> load()async{
-    audioCache.clearCache();
     List<String> listAssetSounds=[Sounds.touch,Sounds.in_correct,Sounds.correct];
-    await audioCache.loadAll(listAssetSounds);
-    // for(String audio in listAssetSounds){
-    //   audioPlayer= await audioCache.play(audio,duckAudio: true, volume: 0, mode: PlayerMode.LOW_LATENCY);
-    // }
+    // await audioCache.loadAll(listAssetSounds);
+    for(String audio in listAssetSounds){
+      audioPlayer= await audioCache.play(audio,duckAudio: true, volume: 0, mode: PlayerMode.LOW_LATENCY);
+    }
   }
 
   static Future<void> play(String audio) async{
