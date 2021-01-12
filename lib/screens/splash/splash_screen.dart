9ec64fe6360c2a7534ec4cd2ea1a0a620b3ed2b6
ThanhTong2810 <tong.duong@kthub.dev>
2021-01-12 09:17:34 +0700

    add dark mode

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:the_enest_english_grammar_test/commons/fade_container.dart';
+import 'package:the_enest_english_grammar_test/controller/app_controller.dart';
 import 'package:the_enest_english_grammar_test/controller/level_controller.dart';
 import 'package:the_enest_english_grammar_test/helper/sounds_helper.dart';
 import 'package:the_enest_english_grammar_test/helper/utils.dart';
@@ -19,6 +20,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen>{
   final LevelController levelController=Get.find();
+  final AppController appController=Get.find();
   Timer timer;
 
   ///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~///
@@ -73,11 +75,19 @@ class _SplashScreenState extends State<SplashScreen>{
   onBuildDone() async {
     await levelController.loadJson();
     await SoundsHelper.load();
+    await checkDarkMode();
     /// Delay 3 seconds, then navigate to Login screen
     timer=Timer.periodic(Duration(seconds: 2), (timer) async {
       await _loadUserData();
       await checkFirstLoad();
     });
+  }
+
+  checkDarkMode() async{
+    final openBox=await Hive.openBox('Dark_Mode');
+    if(openBox.get('isDark')!=null)
+      appController.isDark.value=openBox.get('isDark');
+    openBox.close();
   }
 
   checkFirstLoad()async{
