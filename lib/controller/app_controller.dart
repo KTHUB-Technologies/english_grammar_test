f0c29d75535de48d259d83630d6ac79e22b59c71
kthubDev <53890122+kthubDev@users.noreply.github.com>
2021-01-26 08:17:02 +0700

    login with mic - firebase

--- lib/controller/app_controller.dart
+++ lib/controller/app_controller.dart
@@ -1,24 +1,23 @@
-
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:the_enest_english_grammar_test/helper/config_microsoft.dart';
+import 'package:the_enest_english_grammar_test/helper/firebase_helper.dart';
 import 'package:the_enest_english_grammar_test/helper/shared_preferences_helper.dart';
 import 'package:http/http.dart' as http;
 
-class AppController extends GetxController{
-
+class AppController extends GetxController {
   int value = 0;
 
   bool isEnglish = false;
 
-  Rx<Locale> locale=Rx<Locale>();
+  Rx<Locale> locale = Rx<Locale>();
 
-  Rx<String> idUserMicrosoft=Rx<String>(null);
+  Rx<String> idUserMicrosoft = Rx<String>(null);
 
-  Rx<bool> isShowLoading=Rx<bool>(false);
+  Rx<bool> isShowLoading = Rx<bool>(false);
 
   void reset() {
     value = 0;
@@ -32,13 +31,13 @@ class AppController extends GetxController{
     value--;
   }
 
-  Rx<bool> isDark=Rx<bool>(false);
+  Rx<bool> isDark = Rx<bool>(false);
 
   getDefaultLanguage() async {
-    String languageCode =
-    await SharedPreferencesHelper.getStringValue(SharedPreferencesHelper.LANGUAGE_CODE);
-    if(languageCode.isNotEmpty){
-      switch (languageCode){
+    String languageCode = await SharedPreferencesHelper.getStringValue(
+        SharedPreferencesHelper.LANGUAGE_CODE);
+    if (languageCode.isNotEmpty) {
+      switch (languageCode) {
         case "vi":
           locale.value = Locale("vi", 'VN');
           break;
@@ -62,22 +61,25 @@ class AppController extends GetxController{
     locale.value = Locale("en", 'US');
   }
 
-  loginWithMicrosoft() async{
-    isShowLoading.value=true;
+  loginWithMicrosoft() async {
+    isShowLoading.value = true;
+    await FireBaseHelper.loginWithMicrosoft
+        .then((value) => print(value))
+        .catchError((e) => print(e));
 
-    await ConfigMicrosoft.oauth.login();
-    var accessToken = await ConfigMicrosoft.oauth.getAccessToken();
-    print('Logged in successfully, your access token: $accessToken');
+    // await ConfigMicrosoft.oauth.login();
+    // var accessToken = await ConfigMicrosoft.oauth.getAccessToken();
+    // print('Logged in successfully, your access token: $accessToken');
+    //
+    // final response = await http.get(ConfigMicrosoft.userProfileBaseUrl,headers: {ConfigMicrosoft.authorization: ConfigMicrosoft.bearer + accessToken});
+    // print(response.body);
+    //
+    // Map profile=jsonDecode(response.body);
+    // print(profile['id']);
+    //
+    // idUserMicrosoft.value=profile['id'];
+    // print(idUserMicrosoft.value);
 
-    final response = await http.get(ConfigMicrosoft.userProfileBaseUrl,headers: {ConfigMicrosoft.authorization: ConfigMicrosoft.bearer + accessToken});
-    print(response.body);
-
-    Map profile=jsonDecode(response.body);
-    print(profile['id']);
-
-    idUserMicrosoft.value=profile['id'];
-    print(idUserMicrosoft.value);
-
-    isShowLoading.value=false;
+    isShowLoading.value = false;
   }
 }
