39923ed23274c93a2bd4205b4f8830fb5a80b2b8
kthubDev <53890122+kthubDev@users.noreply.github.com>
2021-02-23 09:31:33 +0700

    fix crash login google

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -8,7 +6,6 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:the_enest_english_grammar_test/app/app.dart';
-import 'package:the_enest_english_grammar_test/controller/user_controller.dart';
 import 'package:the_enest_english_grammar_test/model/question_model.dart';
 
 void main() async {
